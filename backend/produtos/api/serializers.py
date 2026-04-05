b0ed11e29a1b04f2c00c3229a07afd520154da50
lipefp <dinizlipe27@gmail.com>
2026-04-04 22:40:47 -0300

    [fix: resolve pendencias de importação e interpretador python

--- backend/produtos/api/serializers.py
+++ backend/produtos/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Equipamento
+from ..models import Equipamento
 
 class EquipamentoSerializer(serializers.ModelSerializer):
     class Meta:
--- backend/produtos/api/views.py
+++ backend/produtos/api/views.py
@@ -1,7 +1,10 @@
 from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
 from ..models import Equipamento
 from .serializers import EquipamentoSerializer
 
 class EquipamentoViewSet(viewsets.ModelViewSet):
     queryset = Equipamento.objects.all()
     serializer_class = EquipamentoSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['categoria', 'gerenciamento', 'velocidade_uplink']
